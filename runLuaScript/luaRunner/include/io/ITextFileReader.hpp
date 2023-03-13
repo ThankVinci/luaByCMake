4ceb8f7e5efe26ca17d644c2d7a2b6359c105abe
thankvinci <thankvinci@163.com>
2023-03-13 09:03:03 +0800

    文本文件的抽象reader类型新增size属性和获取size的方法

--- runLuaScript/luaRunner/include/io/ITextFileReader.hpp
+++ runLuaScript/luaRunner/include/io/ITextFileReader.hpp
@@ -6,12 +6,15 @@ class ITextFileReader {
 public:
 	ITextFileReader(){
 		readBuff = NULL;
+		readBufferSize = 0;
 	}
 	virtual int readAll(const char* filePath) = 0;
 	const char* getData() { return readBuff;  }
+	size_t getDataSize() { return readBufferSize; }
 	virtual ~ITextFileReader() {
 		if(readBuff != NULL) free(readBuff);
 	}
 protected:
 	char* readBuff;
+	size_t readBufferSize;
 };
