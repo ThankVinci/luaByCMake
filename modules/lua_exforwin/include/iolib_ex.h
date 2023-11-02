2d69e554d19cfe7031eb2a63f831c164652c9c99
ThankVinci <thankvinci@163.com>
2023-11-03 00:14:01 +0800

    iolib_ex模块新增popen

--- modules/lua_exforwin/include/iolib_ex.h
+++ modules/lua_exforwin/include/iolib_ex.h
@@ -3,6 +3,28 @@
 
 #include "efw.h"
 
+#if !defined(l_checkmode)
+
+/* accepted extensions to 'mode' in 'fopen' */
+#if !defined(L_MODEEXT)
+#define L_MODEEXT	"b"
+#endif
+
+/* Check whether 'mode' matches '[rwa]%+?[L_MODEEXT]*' */
+static int l_checkmode(const char* mode) {
+	return (*mode != '\0' && strchr("rwa", *(mode++)) != NULL &&
+		(*mode != '+' || ((void)(++mode), 1)) &&  /* skip if char is '+' */
+		(strspn(mode, L_MODEEXT) == strlen(mode)));  /* check extensions */
+}
+
+#endif
+
+#if !defined(l_checkmodep)
+/* Windows accepts "[rw][bt]?" as valid modes */
+#define l_checkmodep(m)	((m[0] == 'r' || m[0] == 'w') && \
+  (m[1] == '\0' || ((m[1] == 'b' || m[1] == 't') && m[2] == '\0')))
+#endif
+
 int (luaload_io_ex)(lua_State* L);
 
 #endif // _IOLIB_EX_H
--- modules/lua_exforwin/src/iolib_ex.c
+++ modules/lua_exforwin/src/iolib_ex.c
@@ -1,21 +1,5 @@
 #include "iolib_ex.h"
 
-#if !defined(l_checkmode)
-
-/* accepted extensions to 'mode' in 'fopen' */
-#if !defined(L_MODEEXT)
-#define L_MODEEXT	"b"
-#endif
-
-/* Check whether 'mode' matches '[rwa]%+?[L_MODEEXT]*' */
-static int l_checkmode(const char* mode) {
-	return (*mode != '\0' && strchr("rwa", *(mode++)) != NULL &&
-		(*mode != '+' || ((void)(++mode), 1)) &&  /* skip if char is '+' */
-		(strspn(mode, L_MODEEXT) == strlen(mode)));  /* check extensions */
-}
-
-#endif
-
 typedef luaL_Stream LStream;
 
 #define tolstream(L)	((LStream *)luaL_checkudata(L, 1, LUA_FILEHANDLE))
@@ -40,7 +24,6 @@ static LStream* newfile(lua_State* L) {
 	return p;
 }
 
-
 static int io_open_utf8(lua_State* L) {
 	const char* filename = luaL_checkstring(L, 1);
 	const char* mode = luaL_optstring(L, 2, "r");
@@ -55,9 +38,29 @@ static int io_open_utf8(lua_State* L) {
 	return (p->f == NULL) ? luaL_fileresult(L, 0, filename) : 1;
 }
 
+static int io_pclose(lua_State* L) {
+	LStream* p = tolstream(L);
+	errno = 0;
+	return luaL_execresult(L, l_pclose(L, p->f));
+}
+
+static int io_popen(lua_State* L) {
+	const char* filename = luaL_checkstring(L, 1);
+	const char* mode = luaL_optstring(L, 2, "r");
+	wchar_t* w_filename = U8StrtoU16Str(filename);
+	wchar_t* w_mode = U8StrtoU16Str(mode);
+	LStream* p = newprefile(L);
+	luaL_argcheck(L, l_checkmodep(mode), 2, "invalid mode");
+	p->f = _wpopen(w_filename, w_mode);
+	p->closef = &io_pclose;
+	free(w_filename);
+	free(w_mode);
+	return (p->f == NULL) ? luaL_fileresult(L, 0, filename) : 1;
+}
 
 static const luaL_Reg iolib_funcs[] = {
   {"open", io_open_utf8},
+  {"popen", io_open_utf8},
   {NULL, NULL}
 };
 
