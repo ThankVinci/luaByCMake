95f727ce6aa2d664de6bea5c60214145414878d6
ThankVinci <thankvinci@163.com>
2024-01-10 23:14:20 +0800

    demo模块cmake规范化

---
+++ modules/lua_demo/cmake/module.cmake
@@ -0,0 +1,15 @@
+# 包含本项目中的独特配置
+aux_source_directory(${CMAKE_CURRENT_SOURCE_DIR}/src MODULE_SRC_FILES)
+
+if(BUILD_PLATFORM STREQUAL "MinGW")
+add_compile_definitions(LUA_LIB )
+add_compile_options(-pedantic -fpic) #GNU支持的编译器选项
+
+# set(MODULE_LINK_LIBS ${MODULE_LINK_PATH}/libxxx.dll.a )
+elseif(BUILD_PLATFORM STREQUAL "MSVC")
+add_compile_definitions(LUA_LIB )
+
+elseif(BUILD_PLATFORM STREQUAL "LinuxGNU")
+add_compile_options(-pedantic -fpic) #GNU支持的编译器选项
+
+endif()
