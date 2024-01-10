388af22516dc8fa402cf975945dd01ca6b681de9
ThankVinci <thankvinci@163.com>
2024-01-10 23:27:48 +0800

    efw模块cmake规范化

--- modules/lua_exforwin/cmake/module.cmake
+++ modules/lua_exforwin/cmake/module.cmake
@@ -5,8 +5,6 @@ aux_source_directory(${CMAKE_CURRENT_SOURCE_DIR}/src MODULE_SRC_FILES)
 if(BUILD_PLATFORM STREQUAL "MinGW")
 add_compile_definitions(LUA_LIB )
 
-set(MODULE_LINK_LIBS ${MODULE_LINK_PATH}/libiconv.dll.a )
-
 elseif(BUILD_PLATFORM STREQUAL "MSVC")
 add_compile_definitions(LUA_LIB )
 
