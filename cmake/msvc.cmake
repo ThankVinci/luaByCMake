ef675756a2d9c39777349f540c214f7d0bc999e4
ThankVinci <thankvinci@163.com>
2024-02-14 03:01:23 +0800

    MSVC下取消build_type

--- cmake/msvc.cmake
+++ cmake/msvc.cmake
@@ -1,4 +1,6 @@
 if(BUILD_PLATFORM STREQUAL "MSVC") #仅MSVC可用的cmake配置
+set(CMAKE_BUILD_TYPE ) #MSVC取消build_type,交由VS的编译配置自己去管理
+
 add_compile_options(/utf-8)
 add_compile_definitions(LUA_BUILD_AS_DLL ) 
 
