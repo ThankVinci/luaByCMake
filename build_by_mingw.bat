80b0501fa414a520fad7e3183115776e0bb79b1d
ThankVinci <thankvinci@163.com>
2024-06-15 00:45:03 +0800

    修改批处理的注释方式

--- build_by_mingw.bat
+++ build_by_mingw.bat
@@ -1,10 +1,13 @@
+goto start
+= 由于文件提交到git时会自动将换行符转为LF，并且我也不想改变我的配置进而导致整个项目的换行符都改变，
+= 所以如果遇到本批处理文件运行失败的话，请手动将文件的换行符改成CR LF
+= 如果提示找不到make的话，请将编译使用的mingw工具链中的mingw32-make.exe复制一份改名为make.exe
+:start
+
 @echo off
- 
 mkdir build
 cd build
 make clean
 cmake .. -G "MinGW Makefiles"
-::如果提示找不到make的话，请将编译使用的mingw工具链中的mingw32-make.exe复制一份改名为make.exe
-make -j8
-::可以使用make install将编译后的文件安装到项目根目录/installed目录下
+::make -j8
 ::make install
--- build_by_vs.bat
+++ build_by_vs.bat
@@ -1,9 +1,13 @@
+goto start
+= 由于文件提交到git时会自动将换行符转为LF，并且我也不想改变我的配置进而导致整个项目的换行符都改变，
+= 所以如果遇到本批处理文件运行失败的话，请手动将文件的换行符改成CR LF
+= cmake -G 后面表示要构建的项目的生成器类型，这里指定的是vs2022，详情可以使用cmake -G 查看支持的类型
+= 执行完cmake命令后将会在build目录下生成luabycmake的vs项目
+= 使用devenv编译和安装解决方案，需要自己配置devenv的路径
+:start
+
 @echo off
- 
 mkdir build
 cd build
-::-G 后面表示要构建的项目的生成器类型，这里指定的是vs2022，详情可以使用cmake -G 查看支持的类型
 cmake .. -G "Visual Studio 17 2022"
-::执行完cmake命令后将会在build目录下生成luabycmake的vs项目
-::使用devenv编译和安装解决方案，需要自己配置devenv的路径哦
 ::"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe" luaByCMake.sln /Project install.vcxproj /Rebuild Release /out log.txt
