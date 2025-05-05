abafcfd0d61f297316941ec366007714fc0d2e2a
ThankVinci <thankvinci@163.com>
2025-05-05 18:18:26 +0800

    添加图标的安装，把lua的图标安装到lua的安装目录下

--- cmake/mainmodule.cmake
+++ cmake/mainmodule.cmake
@@ -2,6 +2,9 @@
 add_compile_definitions(NDEBUG LUA_COMPAT_5_3 ) # 设置lua中原本的预定义
 add_definitions(-w ) # 隐藏warning
 
+# 设置要安装的图标的路径
+set(LUA_ICO_PATH ${CMAKE_CURRENT_SOURCE_DIR}/lua.ico) 
+
 # 设置要生成的目标的名称以及变量
 set(LIBLUA_STATIC liblua_static)
 set(LIBLUA_SHARED liblua_shared)
@@ -23,3 +26,6 @@ include("${CMAKE_CURRENT_SOURCE_DIR}/cmake/luaexec.cmake")
 # 设置安装头文件
 set(INSTALL_H_FILES ${LUA_INCLUDE_DIR}/lauxlib.h ${LUA_INCLUDE_DIR}/lua.h ${LUA_INCLUDE_DIR}/lua.hpp ${LUA_INCLUDE_DIR}/luaconf.h ${LUA_INCLUDE_DIR}/lualib.h )
 install(FILES ${INSTALL_H_FILES} DESTINATION ${CMAKE_INSTALL_PREFIX}/include)
+
+# 设置安装图标
+install(FILES ${LUA_ICO_PATH} DESTINATION ${CMAKE_INSTALL_PREFIX}/bin)
